f17ef4cc39d5a49a3826a7abb23663b85b412afe
Sodiq <sodiq.akanmu001@gmail.com>
2020-10-15 13:49:33 +0100

    Updates

---
+++ precache-manifest.e15e84fc7b31aa135c6d31a404162b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b361f5362b1ee0a303f86e5cb46101",
+    "url": "/techiesodiq.github.io/sodiq-task/index.html"
+  },
+  {
+    "revision": "ebd7ceb57533c16aa177",
+    "url": "/techiesodiq.github.io/sodiq-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c82a31f7dcf657949fba",
+    "url": "/techiesodiq.github.io/sodiq-task/static/js/2.0250bd54.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/techiesodiq.github.io/sodiq-task/static/js/2.0250bd54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd7ceb57533c16aa177",
+    "url": "/techiesodiq.github.io/sodiq-task/static/js/main.76137c8a.chunk.js"
+  },
+  {
+    "revision": "9a286274489131adc2f1",
+    "url": "/techiesodiq.github.io/sodiq-task/static/js/runtime-main.3d2c60ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techiesodiq.github.io/sodiq-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
